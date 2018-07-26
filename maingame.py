f26191662f200a475b28b657202b4bd7271a597d
allysonja <33104137+allysonja@users.noreply.github.com>
2018-07-26 08:56:00 -0400

    add dictionary url so people won't have to type it

--- maingame.py
+++ maingame.py
@@ -15,6 +15,8 @@ HEIGHT = 400
 WHITE = (255, 255, 255)
 BLACK = (0 ,0, 0)
 
+# http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain
+
 window = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
 pygame.display.set_caption('Hangman')
 
